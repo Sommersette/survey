03ab432a397a5e3f51e2fd32fa14f15b5eec9367
Asia Kane and Trevor Fast <trevor@>
2017-03-22 15:55:12 -0700

    add js rules to collect survey info, store as variables, and display back to user in feedback section upon submission of form

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(function(){
+  $("form#survey").submit(function(event) {
+    event.preventDefault();
+    var nameInput = $("input#name").val();
+    var birthdate = $("input#birthdate").val();
+    var beer = $("input:radio[name=beer]:checked").val();
+    var cuisine = $("#cuisine").val();
+    var color = $("#color").val();
+
+    $("#feedback").text("Thank you " + nameInput + " for your feedback!" );
+    $("#feedback").append(" We've recorded your birthdate as "+ birthdate+ ", your favorite beer as " + beer + ", your favorite cuisine as " + cuisine + " and your favorite hex color as " + color + ".");
+
+  });
+
+});
